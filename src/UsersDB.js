68dd0c5117e4d2be152851a409d23a2d6ade587c
adpol95 <adpol95@gmail.com>
2023-04-25 22:50:06 +0300

    Done with POST method, solution was - First problem was - kanban server doesn't have CORS middleware and by default it cannot take requests from anywhere(CORS policy by default), so, I added CORS in kanban sever and pick for 'Access-Control-Allow-Origin: *' * - get REST operations from anywhere. And the second - I added 'e.preventDefault();' in callback function onClick on button submit

--- src/UsersDB.js
+++ src/UsersDB.js
@@ -1,69 +1,80 @@
 import {useEffect, useState} from "react";
 
 function UsersDB() {
-  // const initialVal = {
-  //   path: 'card',
-  //   method: 'POST',
-  //   body: { name: 'sdasds', email: 'sfascsac' }
-  // }
-  // const [cards, setCards] = useState();
-  // const [errors, setErrors] = useState();
-  const [count, setCounter] = useState(0);
-  // const [fetcher, setFetcher] = useState(initialVal);
-  // const [formName, setFormName] = useState('');
-  // const [formEmail, setFormEmail] = useState('');
+
+  const [cards, setCards] = useState();
+  const [errors, setErrors] = useState();
+  const [formName, setFormName] = useState('');
+  const [formEmail, setFormEmail] = useState('');
 
   useEffect(() => {
-    fetch('https://kanbanexpress1-adpol95.b4a.run/card'
-      // {
-      //   method: 'POST', // *GET, POST, PUT, DELETE, etc.
-      //   body: JSON.stringify({name: 'Test2', email: 'test2@mail.ru'}), // body data type must match "Content-Type
-      //   headers: {
-      //     'Content-type': 'application/json',
-      //   },
-      //   // header
-      // }
-    )
+    fetch('https://kanbanexpress1-adpol95.b4a.run/card')
       .then((res) => res.json())
       .then(
         (result) => {
-          console.log(result);
-          // setCards(result.map(el => <li key={Math.floor(Math.random() * 100) + 1 + ''}>{el.name}</li>))
+          setCards(result.map(el => <li key={Math.floor(Math.random() * 100000) + 1 + ''}>
+            <div>
+              Name: {' '}
+              {el.name}
+            </div>
+            <div>
+              Email: {' '}
+              {el.email}
+            </div>
+            <hr/>
+          </li>))
+        },
+        (errors) => {
+          setErrors(errors)
         })
 
-  }, [])
+  }, [cards])
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('https://kanbanexpress1-adpol95.b4a.run/card', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({name: formName, email: formEmail})
+    })
+      .then(data => data.json())
+      .then((res) => {
+        console.log(res);
+        setFormName('');
+        setFormEmail('');
+      })
+  };
+
   return (
     <div className="fetch-get">
-      {/*<ul>*/}
-      {/*{cards}*/}
-      {/*</ul>*/}
-      <button className="fetch-get__btn fetch-get__btn--minus" onClick={() => setCounter(count - 1)}> Minus</button>
-      {count}
-      <button className="fetch-get__btn fetch-get__btn--plus" onClick={() => setCounter(count + 1)}> Plus</button>
+      <ul>
+        {cards}
+      </ul>
       <hr/>
-      {/*<form >*/}
-      {/*  <label>*/}
-      {/*    <input*/}
-      {/*      type="text"*/}
-      {/*      value={formName}*/}
-      {/*      onChange={(event) => setFormName(event.target.value)}*/}
-      {/*      placeholder={"Type your name"}*/}
-      {/*    />*/}
-      {/*  </label>*/}
-      {/*  <hr/>*/}
-      {/*  <label>*/}
-      {/*    <input*/}
-      {/*      type="text"*/}
-      {/*      value={formEmail}*/}
-      {/*      onChange={(event) => setFormEmail(event.target.value)}*/}
-      {/*      placeholder={"Type your email"}*/}
-      {/*    />*/}
-      {/*  </label>*/}
-      {/*  <hr/>*/}
-      {/*  <button onClick={() => {*/}
-      {/*    setFetcher({...fetcher, method: 'POST', body: JSON.stringify({ name: formName, email: formEmail })})*/}
-      {/*  }}>Add new user</button>*/}
-      {/*</form>*/}
+      <form>
+        <label>
+          <input
+            type="text"
+            value={formName}
+            onChange={(event) => setFormName(event.target.value)}
+            placeholder={"Type your name"}
+          />
+        </label>
+        <hr/>
+        <label>
+          <input
+            type="text"
+            value={formEmail}
+            onChange={(event) => setFormEmail(event.target.value)}
+            placeholder={"Type your email"}
+          />
+        </label>
+        <hr/>
+        <button type="submit" onClick={handleSubmit}>Add new user</button>
+      </form>
+      {JSON.stringify(errors)}
     </div>
   )
 }
